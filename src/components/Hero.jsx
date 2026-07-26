fa42b925b5405f22b8f4612861d40b9084642bde
unknown <allansuarez49@gmail.com>
2026-07-26 09:34:22 +0800

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+function Hero() {  
+    return(
+        <section className="hero">
+            <h3 className="hero-Intro">     
+              SYSTEM ONLINE
+            </h3>
+            <h1 className="hero-Title">
+               Allan 
+            </h1>
+
+            <h4 className="hero-secondaryTitle">
+                Suarez
+                </h4>
+            <h2 className="hero-Subtitle">
+                a Full-Stack Web Developer
+            </h2>
+            <p className="hero-Description">
+                I am a full-stack web developer with a passion for creating dynamic and user-friendly web applications. With a strong foundation in both front-end and back-end technologies, I strive to build seamless digital experiences that meet the needs of users and businesses alike.
+            </p>
+
+            <button className="btn-view"> View My Work</button>
+            <button className="btn-contact"> Contact Me</button>
+        </section>
+    )
+
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+function Navbar() {
+    
+    return (
+        <nav>
+        <h2>
+            Allan L. Suarez
+        </h2>
+        </nav>
+    );
+
+}
+
+export default Navbar;
